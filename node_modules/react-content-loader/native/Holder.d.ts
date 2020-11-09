0d2c634ca7982dc454bd389c1c13becc542bf4b0
Eric Heydorn Gorski <er.h.gorski@gmail.com>
2020-11-09 14:44:20 -0500

    env variable renaming/reorganization

--- node_modules/react-content-loader/native/Holder.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="react" />
-import { IContentLoaderProps } from './';
-export declare const defaultProps: IContentLoaderProps;
-declare const ContentLoader: (props: IContentLoaderProps) => JSX.Element;
-export default ContentLoader;
-//# sourceMappingURL=Holder.d.ts.map
--- node_modules/react-content-loader/native/native/__tests__/offsetValueBound.test.d.ts
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=offsetValueBound.test.d.ts.map
